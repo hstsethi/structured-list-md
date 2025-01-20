f54593de7492927108f650380d8f54d1054e2950
Harshit Sethi <hstsethi@outlook.com>
2025-01-20 10:29:20 +0530

    Update convert script to demonstrate principles of no frills

--- scripts/convert-item-to-def-list.sh
+++ scripts/convert-item-to-def-list.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-sed -i -e 's/<ul>/<dl>/g' \
+# date: 2025-01-20
+# author: HstSethi
+# description: Shell script to convert markdown to Structured HTML list applying principles of Structured List Md
+
+if [ $# -ne 1 ]; then
+
+      echo "Need exactly one argument" >&2
+        exit 1
+fi
+
+# Not using a temp file, Combined sed expressions
+
+cmark $1 | sed -e 's/<ul>/<dl>/g' \
                     -e 's/<\/ul>/<\/dl>/g' \
                     -e 's/<li>/<dt>/g' \
                     -e 's/<\/li>/<\/dt>/g' \
                     -e 's/<p>/<dd>/g' \
-                    -e 's/<\/p>/<\/dd>/g' $1
+                    -e 's/<\/p>/<\/dd>/g'
